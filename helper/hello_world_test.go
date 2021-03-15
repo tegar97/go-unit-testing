03939c94fe6b5524982c16739193a1de0ef92fc1
Muhammad Tegar Akmal <eategar@gmail.com>
2021-03-15 22:45:36 +0700

    func t.falal and t.Error

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("World")
 	if result != "Hello World" {
 		//error
-		t.Fail()
+		t.Error("Error : Result Must Be World")
 	}
+	fmt.Println("finish")
+
 }
 
 func TestHelloWorldTegar(t *testing.T) {
 	result := HelloWorld("Tegar")
 	if result != "Hello Tegar" {
 		//error
-		t.Fail()
+		t.Fatal("Error : Result Must Be Tegar")
+
 	}
+	fmt.Println("finish")
 }
