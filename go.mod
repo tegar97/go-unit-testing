c3a82b6a65d8173241309173c80bc427712db70f
Muhammad Tegar Akmal <eategar@gmail.com>
2021-03-16 09:30:02 +0700

    After Test And Before Test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang-unit-testing
 
 go 1.14
+
+require github.com/stretchr/testify v1.7.0
--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,8 +2,21 @@ package helper
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"runtime"
 	"testing"
 )
+
+func TestMain(m *testing.M) {
+	// BEFORE
+	fmt.Println("Before Unit Test")
+
+	m.Run()
+
+	//after
+	fmt.Println("After Unit Test")
+
+}
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("World")
@@ -17,10 +30,32 @@ func TestHelloWorld(t *testing.T) {
 
 func TestHelloWorldTegar(t *testing.T) {
 	result := HelloWorld("sa")
+
 	if result != "Hello Tegar" {
 		//error
 		t.Fatal("Error : Result Must Be Tegar")
 
 	}
 	fmt.Println("finish")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("World")
+	//if result != "Hello World" {
+	//	//error
+	//	t.Error("Error : Result Must Be World")
+	//}
+	assert.Equal(t, "Hello F", result, "Result Must be 'Hello World'")
+	fmt.Println("finish")
+
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("OS TIDAK SUPPORT")
+	}
+
+	result := HelloWorld("World")
+	assert.Equal(t, "Hello s", result, "Result Must be 'Hello World'")
+
 }
